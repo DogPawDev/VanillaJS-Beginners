90dc19bbcb245944f8ea67929bfdf464cf32d3a3
DogPawDev <foodev@naver.com>
2019-12-19 17:17:11 +0900

    기본 설정

---
+++ index.js
@@ -0,0 +1,16 @@
+//alert('Im Workin. Im JS');
+
+console.log('Im working. im js');
+
+let a = 220;
+let b = a-5;
+
+const hmInfo = {
+    name:"MinSu",
+    age: 22,
+    gender:"Male",
+    isHandsome:true
+}
+
+console.log(hmInfo);
+console.log(hmInfo.gender);
