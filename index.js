0a85a61f48c5339e41330cef00879f6cc1d2f75f
DogPawDev <foodev@naver.com>
2020-01-12 18:25:09 +0900

    함수

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //alert('Im Workin. Im JS');
 
 console.log('Im working. im js');
-
+/*
 let a = 220;
 let b = a-5;
 
@@ -14,3 +14,38 @@ const hmInfo = {
 
 console.log(hmInfo);
 console.log(hmInfo.gender);
+*/
+
+const hkInfo ={
+    name: "MinSu",
+    age: 20,
+    gender:"male",
+    favMovies: ["Along the gods","LOTR","OldBoy"],
+    favFood:[{
+        name:"kimchi",
+        fatty:false
+    },
+    {
+        name:"cheese burger",
+        fatty:true
+    }]
+}
+
+console.log(hkInfo.favFood[0].name);
+
+function sayHello(name,age){
+    console.log(`Hello ${name} you are ${age} years old`);
+}
+
+sayHello("dd",13);
+
+
+const calcuator ={
+    plus:function(a,b){
+        return a+b;
+    }
+
+}
+
+const plus = calcuator.plus(4,5);
+console.log(plus);
