073fa67518f2071c486be6e7dea7789258127306
DogPawDev <foodev@naver.com>
2020-01-30 15:55:45 +0900

    추가

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const hmInfo = {
 
 console.log(hmInfo);
 console.log(hmInfo.gender);
-*/
+
 
 const hkInfo ={
     name: "MinSu",
@@ -49,3 +49,48 @@ const calcuator ={
 
 const plus = calcuator.plus(4,5);
 console.log(plus);
+
+
+const title = document.getElementById("title");
+
+function handleResize(){
+    title.style.color = "blue";
+}
+
+title.addEventListener("click",handleResize);
+
+
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(){
+    const currentColor = title.style.color;
+    if(currentColor === BASE_COLOR){
+        title.style.color=OTHER_COLOR;
+    }else{
+        title.style.color=BASE_COLOR;
+    }
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click",handleClick);
+}
+
+init();
+*/
+
+const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+   title.classList.toggle(CLICKED_CLASS);
+
+}
+
+function init(){
+    title.addEventListener("click",handleClick);
+}
+init();
